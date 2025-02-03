ebd0f7dfa0c00bd0547273e9623673d5af21c83e
Justin Saye <jzsaye@outlook.com>
2025-02-02 21:12:52 -0600

    Added game engine, integrated with tournament.py.

--- tournament.py
+++ tournament.py
@@ -1,23 +1,7 @@
 import random
 from itertools import combinations
+from game_engine import RandomPlayer, Game
 #Has built in function to return cross product
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-    
-class Game:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-
-    def play(self):
-        #Simulates a game
-        winner = random.choice([self.player1, self.player2])
-        return winner
 
 class Tournament:
     def __init__(self, players, games_per_match):
@@ -31,9 +15,10 @@ class Tournament:
         for _ in range(self.games_per_match):
             for player1, player2 in matchups:
                 game = Game(player1, player2)
-                winner = game.play()
+                winner = game.start()
                 print(f"{player1} vs {player2} winner: {winner}")
 
-players = [Player("A"), Player("B"), Player("C"), Player("D"), Player("T")]
-tournament = Tournament(players, games_per_match=3)
-tournament.run()
+if __name__ == '__main__':
+    players = [RandomPlayer("A"), RandomPlayer("B"), RandomPlayer("C"), RandomPlayer("D"), RandomPlayer("T")]
+    tournament = Tournament(players, games_per_match=3)
+    tournament.run()
